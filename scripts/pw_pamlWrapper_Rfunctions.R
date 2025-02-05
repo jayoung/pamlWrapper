01e57356838c00337c491ef464343006b7ba486b
Janet Young <jayoung@fredhutch.org>
2025-02-04 17:35:48 -0800

    add simplify_sites_strings R function

--- scripts/pw_pamlWrapper_Rfunctions.R
+++ scripts/pw_pamlWrapper_Rfunctions.R
@@ -88,3 +88,27 @@ readPAMLresultsWideCombined <- function(tsv_file,
     return(output)
 }
 
+###### simplify_sites_strings - a little function, to take the sites column in the output of PAML pipeline (looks like this: "75_N_0.979,99_D_0.905,358_T_0.914") and to return only the site ID and the amino acid (e.g. "75N,99D,358T")
+simplify_sites_strings <- function(sites_strings) {
+    ## strsplit will return an NA if input was NA
+    ## each_site is a list, one element per entry in the sites table, 
+    ## each element is a character vector of the info for each site
+    each_site <- strsplit(sites_strings, ",")
+    each_site_simple <- lapply(each_site, function(x) {
+        each_site_split <- strsplit(x, "_")
+        each_analysis_simple <- sapply(each_site_split, function(y) {
+            if(is.na(y[1])) {return(NA)}
+            new_site <- paste(y[1:2], collapse="")
+            return(new_site)
+        }, USE.NAMES = FALSE) 
+        each_analysis_simple <- paste(each_analysis_simple, collapse=",")
+        return(each_analysis_simple)
+    })
+    return(each_site_simple)
+}
+
+
+## test readPAMLresultsWide
+# temp <- readPAMLresultsWide("zz_allAlignments.PAMLsummaries.allParams.wide.tsv")
+# temp2 <- simplify_sites_strings(temp$m8_which_sites_have_beb_0_9)
+# temp2
